0ec7a1cc9da61a8e4458afe00b0d9b2e140b50bb
Jayphen <jayphen@gmail.com>
2017-04-13 22:01:14 +0200

    Use URLSearchParams object instead of custom methods

--- background.js
+++ background.js
@@ -7,19 +7,6 @@
 if(window.location.href.indexOf(".airbnb.") > -1 && window.location.href.indexOf("/s/") > -1 ) {
 
     $(document).ready(function() {
-        getVal = function(str, start, end) {
-            var startPos = str.indexOf(start);
-            var endPos = str.indexOf(end);
-
-            if (end == '') {
-                endPos = str.length;
-            }
-
-            var val = '';
-            val = str.substring(startPos + start.length, endPos);
-            return val;
-        };
-
         getMonth = function(date) {
           var month = date.getMonth() + 1;
           return month < 10 ? '0' + month : '' + month; // ('' + month) for string result
@@ -46,17 +33,18 @@ if(window.location.href.indexOf(".airbnb.") > -1 && window.location.href.indexOf
         var check_in_price_format = '';
         var check_out = '';
         var check_out_price_format = '';
-        var guests = '';
-
+        var guests = '1';
 
 
         $('[class*=listingCardWrapper]:first').each(function() {
             var url = $(this).find('[class^=anchor_]').attr('href');
-            var paramguests = getVal(url, '&guests=', '&adults=');
+            var urlParams = new URLSearchParams(url.split('?')[1]);
+            var paramguests = urlParams.get('guests');
 
-            check_in = decodeURIComponent(getVal(url, '?checkin=', '&checkout='));
-            check_out = decodeURIComponent(getVal(url, '&checkout=', '&guests='));
-            guests = isNaN(paramguests) ? 1 : paramguests;
+            check_in = urlParams.get('checkin');
+            check_out = urlParams.get('checkout');
+            guests = paramguests == undefined ? 1 : paramguests;
+
             // number_of_adults = getVal(url, '&adults=', '&children=');
             // number_of_children = getVal(url, '&children=', '&infants=');
             // number_of_infants = url.substring(url.indexOf('&infants=') + 9);
@@ -85,42 +73,6 @@ if(window.location.href.indexOf(".airbnb.") > -1 && window.location.href.indexOf
                 x1 = x1.replace(rgx, '$1' + ',' + '$2');
             }
             return x1 + x2;
-        };
-
-        updatePrices = function() {
-            $('[class*=listingCardWrapper]:first').each(function() {
-                var url = $(this).find('[class^=anchor_]').attr('href');
-
-                var listing_id = getVal(url, '/rooms/', '?checkin=');
-                var check_in = getVal(url, '?checkin=', '&checkout=');
-                var check_out = getVal(url, '&checkout=', '&guests=');
-                var guests = getVal(url, '&guests=', '&adults=');
-
-                check_in = check_in.split('-');
-                check_in = check_in[2] + '-' + check_in[1] + '-' + check_in[0];
-
-                check_out = check_out.split('-');
-                check_out = check_out[2] + '-' + check_out[1] + '-' + check_out[0];
-
-                var priceUrl = domain + "/api/v2/pricing_quotes?guests=" + guests + "&listing_id=" + listing_id + "&_format=for_detailed_booking_info_on_web_p3_with_message_data&_interaction_type=pageload&_intents=p3_book_it&show_smart_promotion=0&check_in=" + check_in + "&check_out=" + check_out + "&key=" + key + "&currency=" + currency + "&locale=en-US";
-
-                $.getJSON( priceUrl, function( data ) {
-                    var totalPrice = data.pricing_quotes[0].price.total.amount;
-                    var currentPrice = $('a[href="' + url + '"]').parent().find('span.price-amount').html();
-                    currentPrice = currentPrice + ' (' + addCommas(totalPrice) + ' total)';
-                    $('a[href="' + url + '"]').parent().find('span.price-amount').html(currentPrice);
-                });
-
-                $(this).removeClass('listing-card-wrapper');
-
-            });
-        };
-
-        updatePricesLoop = function() {
-            updatePrices();
-            setTimeout(function () {
-                updatePricesLoop();
-            }, 1000);
         };
 
         addRoom = function(room) {
@@ -208,64 +160,6 @@ if(window.location.href.indexOf(".airbnb.") > -1 && window.location.href.indexOf
 
         };
 
-        updateQueryStringParameter = function(uri, key, value) {
-          var re = new RegExp("([?&])" + key + "=.*?(&|#|$)", "i");
-          if (uri.match(re)) {
-            return uri.replace(re, '$1' + key + "=" + value + '$2');
-          } else {
-            var hash =  '';
-            if( uri.indexOf('#') !== -1 ){
-                hash = uri.replace(/.*#/, '#');
-                uri = uri.replace(/#.*/, '');
-            }
-            var separator = uri.indexOf('?') !== -1 ? "&" : "?";
-            return uri + separator + key + "=" + value + hash;
-          }
-        };
-
-        getParameterByName = function (name, url) {
-            if (!url) {
-              url = window.location.href;
-            }
-            name = name.replace(/[\[\]]/g, "\\$&");
-            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-                results = regex.exec(url);
-            if (!results) return null;
-            if (!results[2]) return '';
-            return decodeURIComponent(results[2].replace(/\+/g, " "));
-        };
-
-        updateQuerystringValues = function(urlSource, urlTarget, params) {
-            for (var i=0; i<params.length; i++) {
-                var param = params[i];
-                var newValue = getParameterByName(param, urlTarget);
-
-                if (newValue != null) {
-                    urlSource = updateQueryStringParameter(urlSource, param, newValue);
-                }
-            }
-
-            return urlSource;
-        };
-
-        removeParameter = function(url, parameter) {
-          var urlparts= url.split('?');
-
-          if (urlparts.length>=2)
-          {
-              var urlBase=urlparts.shift(); //get first part, and remove from array
-              var queryString=urlparts.join("?"); //join it back up
-
-              var prefix = encodeURIComponent(parameter)+'=';
-              var pars = queryString.split(/[&;]/g);
-              for (var i= pars.length; i-->0;)               //reverse iteration as may be destructive
-                  if (pars[i].lastIndexOf(prefix, 0)!==-1)   //idiom for string.startsWith
-                      pars.splice(i, 1);
-              url = urlBase+'?'+pars.join('&');
-          }
-          return url;
-        };
-
         getListings = function() {
             $('#airtotal').css('display', 'block');
             $('#airtotal div.listing').remove();
@@ -289,19 +183,20 @@ if(window.location.href.indexOf(".airbnb.") > -1 && window.location.href.indexOf
             var metaUrl = $('meta[property="al:ios:url"]').attr('content');
             metaUrl = metaUrl.replace('check_in', 'checkin');
             metaUrl = metaUrl.replace('check_out', 'checkout');
-            var searchParams = '/search/search_results?' + metaUrl.substring(18) + '&location=' + $('#header-search-form').val();
+            var searchParams = '?' + metaUrl.substring(18) + '&location=' + $('.copy_14aozyc').eq(0).text();
             searchParams = decodeURIComponent(searchParams);
+            searchParams = new URLSearchParams(searchParams);
 
-            var checkinVal = getVal(searchParams, '&checkin=', '&checkout=');
-            var checkoutVal = getVal(searchParams, '&checkout=', '&children=');
+            var checkinVal = searchParams.get('checkin');
+            var checkoutVal = searchParams.get('checkout');;
 
             var checkinArr = checkinVal.split('/');
             var checkoutArr = checkoutVal.split('/');
 
             var newCheckinVal = checkinArr[2] + '-' + checkinArr[1] + '-' + checkinArr[0];
             var newCheckoutVal = checkoutArr[2] + '-' + checkoutArr[1] + '-' + checkoutArr[0];
-            searchParams = searchParams.replace(checkinVal, newCheckinVal);
-            searchParams = searchParams.replace(checkoutVal, newCheckoutVal);
+            searchParams.set('checkin', newCheckinVal);
+            searchParams.set('checkout', newCheckoutVal);
 
             var paramNames = [
                 'adults',
@@ -320,12 +215,20 @@ if(window.location.href.indexOf(".airbnb.") > -1 && window.location.href.indexOf
                 'zoom'
             ];
 
-            searchParams = updateQuerystringValues(searchParams, document.URL, paramNames);
-            searchParams = removeParameter(searchParams, 'page');
+            var currentUrl = new URLSearchParams(window.location.search);
+            for (var i=0; i<paramNames.length; i++) {
+                // Merge any current search terms
+                paramVal = currentUrl.get(paramNames[i]);
+                if (paramVal !== null) {
+                    searchParams.set(paramNames[i], paramVal);
+                }
+            }
 
             var urls = [nbPages-1];
             for (var i=0; i<nbPages; i++) {
-                urls[i] = searchParams + '&page=' + (i + 1);
+                var newUrl = searchParams;
+                newUrl.set('page', i+1);
+                urls[i] = "search/search_results?" + newUrl.toString();
             }
 
             for (var j=0; j<nbPages; j++) {
